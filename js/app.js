cc9607d95672503bc435fd8f075dfc99dca2e8e5
Mahendhar <mahendarreddy@live.com>
2015-09-10 02:00:22 +0530

    Using OpenWeatherMap API to get the weather forecast for the number of days passed in the params.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,11 @@ weatherApp.config(function ($routeProvider) {
     templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
   })
+  
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  })
 
 });
 
@@ -31,7 +36,42 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
   });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
-    $scope.city = cityService.city
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI =           $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"},{get: {method: "JSONP"}});
+  
+  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days });
+  
+  console.log($scope.weatherResult);
+  
+  $scope.convertToCelsius = function(kelvin){
+    return Math.round(kelvin - 273.15); 
+  };
+  
+  $scope.convertToDate = function(dt){
+    return new Date(dt*1000);
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }]);
 
